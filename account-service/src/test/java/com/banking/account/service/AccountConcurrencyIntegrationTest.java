bbbfb7592c0b968d81ef34d41c9f301f537b4674
h2a <hung.hoanganh@gotadi.com>
2026-01-16 23:51:41 +0700

    fix unit test

--- account-service/src/test/java/com/banking/account/service/AccountConcurrencyIntegrationTest.java
+++ account-service/src/test/java/com/banking/account/service/AccountConcurrencyIntegrationTest.java
@@ -12,6 +12,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.math.BigDecimal;
@@ -25,6 +26,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@EmbeddedKafka(partitions = 1)
 public class AccountConcurrencyIntegrationTest {
 
     @Autowired
